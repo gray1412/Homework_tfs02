07719cbec0257d439d509492fe07418a26f58633
Ngochd7 <70498933+Ngochd7@users.noreply.github.com>
2021-06-15 22:34:27 +0700

    Update main.go

--- NguyenDinhNgoc/AssLec4/main.go
+++ NguyenDinhNgoc/AssLec4/main.go
@@ -24,7 +24,9 @@ func downloadWithGoroutine(urls []string) (MapFilm storage.Memory_Storage_Film, 
 }
 func main() {
 
-	urls := []string{"https://www.imdb.com/chart/top/?ref_=nv_mv_250", "https://template-homedecor.onshopbase.com/api/catalog/products_v2.json?sort_field=name&sort_direction=asc&limit=12&page=1&collection_ids=86733892580&fbclid=IwAR2aOwNN4HuCMN2V6Lv5A7MehtD47kpX9Bt4wBs--OC72vNllI5S-eRSLDw"}
+	urls := []string{"https://www.imdb.com/chart/top/?ref_=nv_mv_250",
+			 "https://template-homedecor.onshopbase.com/api/catalog/products_v2.json?sort_field=name&sort_direction=asc&limit=12&page=1&collection_ids=86733892580&fbclid=IwAR2aOwNN4HuCMN2V6Lv5A7MehtD47kpX9Bt4wBs--OC72vNllI5S-eRSLDw"
+		}
 
 	MapFilm, MapItem := downloadWithGoroutine(urls)
 	fmt.Println(MapFilm)

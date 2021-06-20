d03cdc66ed5b475c84c2e542006758224eb21b9b
phamdat16041999 <56620800+phamdat16041999@users.noreply.github.com>
2021-06-20 22:10:00 +0700

    Update students.go

--- Homeword Pham_Thanh_Dat/lec-06/students/students.go
+++ Homeword Pham_Thanh_Dat/lec-06/students/students.go
@@ -11,10 +11,19 @@ import (
 
 func PathStudent() {
 	r := mux.NewRouter().StrictSlash(true)
-	r.Methods(http.MethodPost).Path("/students").HandlerFunc(addStudent)
-	r.Methods(http.MethodPut).Path("/students").HandlerFunc(updateStudent)
-	r.Methods(http.MethodDelete).Path("/students").HandlerFunc(deleteStudent)
-	r.Use(contentTypeJson)
+
+	get := r.Methods(http.MethodGet).Subrouter()
+	get.Path("/students").HandlerFunc(viewStudent)
+	get.Use(contentTextPlan)
+	post := r.Methods(http.MethodPost).Subrouter()
+	post.Path("/students").HandlerFunc(addStudent)
+	post.Use(contentTypeJson)
+	put := r.Methods(http.MethodPut).Subrouter()
+	put.Path("/students").HandlerFunc(updateStudent)
+	put.Use(contentTypeJson)
+	delete := r.Methods(http.MethodDelete).Subrouter()
+	delete.Path("/students").HandlerFunc(deleteStudent)
+	delete.Use(contentTypeJson)
 	http.Handle("/", r)
 	http.ListenAndServe(":8000", nil)
 }
@@ -22,6 +31,12 @@ func PathStudent() {
 // func viewPerson(w http.ResponseWriter, r *http.Request) {
 // 	fmt.Fprintf(w, "Student: %+v", Api.Students)
 // }
+func viewStudent(w http.ResponseWriter, r *http.Request) {
+	db := Api.Connect()
+	student := &Api.Students{}
+	db.Find(&student)
+	fmt.Printf("%+v", student)
+}
 func addStudent(w http.ResponseWriter, r *http.Request) {
 	var s Api.Students
 	err := json.NewDecoder(r.Body).Decode(&s)
@@ -71,6 +86,11 @@ func contentTypeJson(next http.Handler) http.Handler {
 			fmt.Fprintf(w, "Only allow request with content type %v", contentType)
 			return
 		}
+		next.ServeHTTP(w, r)
+	})
+}
+func contentTextPlan(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		next.ServeHTTP(w, r)
 	})
 }
